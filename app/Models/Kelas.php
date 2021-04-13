d655e8910fb5f2ab98278cf7f8ec740ffcb8bba1
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-04-13 11:31:51 +0700

    Praktikum No.17 mengedit file model kelas

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class kelas extends Model
+class Kelas extends Model
 {
     use HasFactory;
+    protected $table = 'kelas';
+
+    public function mahasiswa(){
+        return $this->hasMany(Mahasiswa::class);
+    }
 }
