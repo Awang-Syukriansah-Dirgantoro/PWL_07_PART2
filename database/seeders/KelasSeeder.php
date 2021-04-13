5d53281635e803d6b43381b12d1afe77142eb339
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-04-13 11:06:30 +0700

    Praktikum No.06 mengedit file kelas seeder

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KelasSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class KelasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $kelas = [
+            ['Nama_Kelas' => 'TI 2A'],
+            ['Nama_Kelas' => 'TI 2B'],
+            ['Nama_Kelas' => 'TI 2C'],
+            ['Nama_Kelas' => 'TI 2D'],
+            ['Nama_Kelas' => 'TI 2E'],
+            ['Nama_Kelas' => 'TI 2F'],
+            ['Nama_Kelas' => 'TI 2G'],
+            ['Nama_Kelas' => 'TI 2H'],
+            ['Nama_Kelas' => 'TI 2I'],
+        ];
+
+        DB::table('kelas')->insert($kelas);
     }
 }
